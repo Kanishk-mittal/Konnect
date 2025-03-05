0fc4a12d6e745b9cddda6f7dfb9b5386a9cba9df
AadiBagde <siddhantBagde5431@gmail.com>
2025-03-05 16:54:53 +0530

    Create clubs.js

---
+++ Backend/Models /clubs.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const clubSchema = new mongoose.Schema({
+  club_Name : {type : String , required : true },
+  Creator_Id : {type : mongoose.Schema.Types.ObjectId ,ref : "User", required : true},
+  Created_time : {type : Date , default :Date.now} , 
+  Updated_time : {type : Date , default : Date.now },
+})
+
+module.exports = mongoose.exports("Clubs",clubSchema);
