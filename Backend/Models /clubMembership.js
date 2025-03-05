84e92cd93a6e1ab23c11fd006737c147a113583e
AadiBagde <siddhantBagde5431@gmail.com>
2025-03-05 17:07:29 +0530

    Create clubMembership.js

---
+++ Backend/Models /clubMembership.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const clubMembershipSchema = new mongoose.Schema({
+    Club_id : {type : mongoose.Schema.Types.ObjectId,ref : "Clubs", required : true},
+    User_id : {type : mongoose.Schema.Types.ObjectId , ref :"User" , required : true},
+    Role : {type : String , enum : ["Member" ,"Admin"] , required : true},
+    joined_at :{type : Date , default : Date.now} ,
+    invitationStatus : {type : String , enum : ["requested","pending" ,"accepted"] ,default : "pending"},
+
+});
+
+module.exports = mongoose.exports("clubsMem", clubMembershipSchema);
