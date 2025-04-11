8469c1276e723a6e8f87cfb3b2f048ef7c4f08bf
Kanishk-mittal <kanishkmittal2005@gmail.com>
2025-04-11 10:13:49 +0530

    Clearing board for new version

--- Konnect/Integration/apiConfig.js
+++
@@ -1,4 +0,0 @@
-// Ensure API_BASE_URL points to the correct server
-const API_BASE_URL = 'http://localhost:5000';
-
-export default API_BASE_URL;
--- Konnect/Integration/temp.js
+++
@@ -1,9 +0,0 @@
-import CryptoJS from 'crypto-js';
-
-
-
-
-const message = "Wmv91qJtWWowsO81gDe+nmvvN0NjgiTc6IVylPEU+uI= ";
-const key = "fiHrEqITh/Shkyypx06unw==";
-
-console.log(decryptWithAES(message, key));
